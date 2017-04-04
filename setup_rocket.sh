b1a1d67e49e429a5ca203ff7c49fc58d4ef41fdb
stackaero <stackaero@gmail.com>
2017-04-03 19:42:40 -0500

    Update setup_rocket.sh

--- setup_rocket.sh
+++ setup_rocket.sh
@@ -3,4 +3,5 @@
 #downloading rocket chat client
 echo installing rocket chat
 wget https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.6.0/rocketchat_2.6.0_amd64.deb
-dpkg -i rocketchat_2.6.0_amd64.deb
+sudo dpkg -i rocketchat_2.6.0_amd64.deb
+sudo apt-get install code
