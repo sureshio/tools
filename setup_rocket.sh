ffd498a9c3372df4e64b4b40c021bc9ce03f2500
stackaero <stackaero@gmail.com>
2017-04-03 19:40:58 -0500

    Create setup_rocket.sh

---
+++ setup_rocket.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#downloading rocket chat client
+echo installing rocket chat
+wget https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.6.0/rocketchat_2.6.0_amd64.deb
+dpkg -i rocketchat_2.6.0_amd64.deb
