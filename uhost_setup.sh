8273824f3abd633c7272cb8d13500709e904f36b
stackaero <stackaero@gmail.com>
2017-04-03 20:37:34 -0500

    Update uhost_setup.sh

--- uhost_setup.sh
+++ uhost_setup.sh
@@ -56,3 +56,6 @@ curl -fsSL https://get.docker.com | sh
 #cd .. && rm -rf install_tmp
 
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
