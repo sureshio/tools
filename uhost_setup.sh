df5b010f6ba71d0d56506f1323b0f437467823dd
stackaero <stackaero@gmail.com>
2017-04-03 19:21:03 -0500

    Update uhost_setup.sh

--- uhost_setup.sh
+++ uhost_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get -y update
+
 # Install Node.Js
 
 NODE_VER=5.5.0
@@ -13,4 +15,37 @@ ln -s /opt/nodejs-v${NODE_VER}/bin/node /usr/bin/node
 ln -s /opt/nodejs-v${NODE_VER}/bin/npm /usr/bin/npm
 node-${NODE_VER} -v
 rm /tmp/node-v${NODE_VER}.tar.gz
+
+echo Installing Git wget
+apt-get install -y git
+
+echo Installing Docker
+curl -fsSL https://get.docker.com | sh
+
+echo installing ansible
+apt-get install ansible
+
+#installing robo mongo
+wget https://download.robomongo.org/1.0.0-rc1/linux/robomongo-1.0.0-rc1-linux-x86_64-496f5c2.tar.gz
+tar xf robomongo-1.0.0-rc1-linux-x86_64-496f5c2.tar.gz
+#sudo mv robomongo /usr/bin/robomongo
+
+#downloading rocket chat client
+wget https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.6.0/rocketchat_2.6.0_amd64.deb
+
+echo Installing Visual Studio Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+apt-get update
+apt-get install -y code # or code-insiders
+
+
+echo Installing zoom plugin
+wget https://zoom.us/client/latest/zoom_amd64.deb
+
+dpkg -i zoom_amd64.deb
+apt-get install -f
+
 
