cf390ff926a96e540c58461d351a131cc963fe67
stackaero <stackaero@gmail.com>
2017-04-07 03:46:09 -0500

    Update setupnfs.sh

--- setupnfs.sh
+++ setupnfs.sh
@@ -3,3 +3,5 @@ sudo apt-get -y install nfs-common portmap
 
 sudo mkdir /opt/aerobox
 echo "nfs.stackaero.io:/aerobox   /opt/aerobox  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
+
+sudo mount -a
